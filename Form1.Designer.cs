f0710da494b0d9aeb952046673bb877b94a98e43
Feared Heart <lost.data@hotmail.com>
2023-09-27 15:57:46 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Non_Rectangular_Shaped_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.close = new System.Windows.Forms.Button();
+            this.panelContainer.SuspendLayout();
+            this.panelTitleBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(297, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.Color.LavenderBlush;
+            this.panelContainer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panelContainer.Controls.Add(this.panelTitleBar);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.panelContainer.Location = new System.Drawing.Point(0, 0);
+            this.panelContainer.Margin = new System.Windows.Forms.Padding(0);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(700, 338);
+            this.panelContainer.TabIndex = 1;
+            this.panelContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContainer_Paint);
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panelTitleBar.Controls.Add(this.close);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(700, 50);
+            this.panelTitleBar.TabIndex = 0;
+            this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown_1);
+            // 
+            // close
+            // 
+            this.close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.close.FlatAppearance.BorderSize = 0;
+            this.close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.close.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.close.Location = new System.Drawing.Point(625, 0);
+            this.close.Margin = new System.Windows.Forms.Padding(0);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(75, 50);
+            this.close.TabIndex = 0;
+            this.close.Text = "X";
+            this.close.UseVisualStyleBackColor = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.panelContainer);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panelContainer.ResumeLayout(false);
+            this.panelTitleBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private System.Windows.Forms.Button close;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+
+namespace Non_Rectangular_Shaped_Forms
+{
+    public partial class Form1 : Form
+    {
+        private int borderRadius = 20;
+        private int borderSize = 3;
+        private Color borderColor = Color.Orange; //Color.FromArgb(128, 128, 255);
+        private Timer timerFormReactivate = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Padding = new Padding(borderSize);
+            this.panelTitleBar.BackColor = borderColor;
+            this.BackColor = borderColor;
+            timerFormReactivate.Interval = 500;
+            timerFormReactivate.Tick += new EventHandler(TimerEventProcessor);
+            //this.BackColor = borderColor;
+        }
+
+        private void TimerEventProcessor(object sender, EventArgs e)
+        {
+            timerFormReactivate.Stop();
+            this.Invalidate();
+            // MessageBox.Show("Done");
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //DrawImagePointF(e);
+            /*
+            //-> SMOOTH OUTER BORDER
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rectForm = this.ClientRectangle;
+            int mWidht = rectForm.Width / 2;
+            int mHeight = rectForm.Height / 2;
+            var fbColors = GetFormBoundsColors();
+            //Top Left
+            DrawPath(rectForm, e.Graphics, fbColors.TopLeftColor);
+            //Top Right
+            Rectangle rectTopRight = new Rectangle(mWidht, rectForm.Y, mWidht, mHeight);
+            DrawPath(rectTopRight, e.Graphics, fbColors.TopRightColor);
+            //Bottom Left
+            Rectangle rectBottomLeft = new Rectangle(rectForm.X, rectForm.X + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomLeft, e.Graphics, fbColors.BottomLeftColor);
+            //Bottom Right
+            Rectangle rectBottomRight = new Rectangle(mWidht, rectForm.Y + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomRight, e.Graphics, fbColors.BottomRightColor);
+            */
+            //-> SET ROUNDED REGION AND BORDER
+
+            // Test
+            //this.panelTitleBar.Visible = false;
+            //this.panelContainer.Visible = false;
+            // this.close.Visible = false;
+            //
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rectForm = this.ClientRectangle;
+            int mWidht = rectForm.Width / 2;
+            int mHeight = rectForm.Height / 2;
+            var fbColors = GetFormBoundsColors();
+
+            DrawPath(rectForm, e.Graphics, fbColors.TopLeftColor);
+
+            Rectangle rectTopRight = new Rectangle(mWidht, rectForm.Y, mWidht, mHeight);
+            DrawPath(rectTopRight, e.Graphics, fbColors.TopRightColor);
+
+            Rectangle rectBottomLeft = new Rectangle(rectForm.X, rectForm.X + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomLeft, e.Graphics, fbColors.BottomLeftColor);
+
+            Rectangle rectBottomRight = new Rectangle(mWidht, rectForm.Y + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomRight, e.Graphics, fbColors.BottomRightColor);
+
+            FormRegionAndBorder(this, borderRadius, e.Graphics, borderColor, borderSize);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            /*
+            base.OnPaintBackground(e);
+
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rectForm = this.ClientRectangle;
+            int mWidht = rectForm.Width / 2;
+            int mHeight = rectForm.Height / 2;
+            var fbColors = GetFormBoundsColors();
+
+            DrawPath(rectForm, e.Graphics, fbColors.TopLeftColor);
+
+            Rectangle rectTopRight = new Rectangle(mWidht, rectForm.Y, mWidht, mHeight);
+            DrawPath(rectTopRight, e.Graphics, fbColors.TopRightColor);
+
+            Rectangle rectBottomLeft = new Rectangle(rectForm.X, rectForm.X + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomLeft, e.Graphics, fbColors.BottomLeftColor);
+
+            Rectangle rectBottomRight = new Rectangle(mWidht , rectForm.Y + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomRight, e.Graphics, fbColors.BottomRightColor);
+            */
+        }
+
+        private struct FormBoundsColors
+        {
+            public Color TopLeftColor;
+            public Color TopRightColor;
+            public Color BottomLeftColor;
+            public Color BottomRightColor;
+        }
+        private void DrawImagePointF(PaintEventArgs e)
+        {
+
+            // Create image.
+            Image newImage = Image.FromFile(@"C:\Users\User Abdul\source\repos\Non-Rectangular-Shaped_Forms\pliers1.jpg");
+
+            // Create point for upper-left corner of image.
+            PointF ulCorner = new PointF(100.0F, 100.0F);
+
+            // Draw image to screen.
+            e.Graphics.DrawImage(newImage, ulCorner);
+        }
+
+
+        private GraphicsPath GetRoundedPath(Rectangle rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+        private FormBoundsColors GetFormBoundsColors()
+        {
+            var fbColor = new FormBoundsColors();
+            using (var bmp = new Bitmap(1, 1))
+            using (Graphics graph = Graphics.FromImage(bmp))
+            {
+                Rectangle rectBmp = new Rectangle(0, 0, 1, 1);
+                //Top Left
+                rectBmp.X = this.Bounds.X - 1;
+                rectBmp.Y = this.Bounds.Y;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.TopLeftColor = bmp.GetPixel(0, 0);
+                //Top Right
+                rectBmp.X = this.Bounds.Right;
+                rectBmp.Y = this.Bounds.Y;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.TopRightColor = bmp.GetPixel(0, 0);
+                //Bottom Left
+                rectBmp.X = this.Bounds.X;
+                rectBmp.Y = this.Bounds.Bottom;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.BottomLeftColor = bmp.GetPixel(0, 0);
+                //Bottom Right
+                rectBmp.X = this.Bounds.Right;
+                rectBmp.Y = this.Bounds.Bottom;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.BottomRightColor = bmp.GetPixel(0, 0);
+            }
+            return fbColor;
+        }
+
+        private void FormRegionAndBorder(Form form, float radius, Graphics graph, Color borderColor, float borderSize)
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                using (GraphicsPath roundPath = GetRoundedPath(form.ClientRectangle, radius))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                using (Matrix transform = new Matrix())
+                {
+                    graph.SmoothingMode = SmoothingMode.AntiAlias;
+                    form.Region = new Region(roundPath);
+                    if (borderSize >= 1)
+                    {
+                        Rectangle rect = form.ClientRectangle;
+                        float scaleX = 1.0F - ((borderSize + 1) / rect.Width);
+                        float scaleY = 1.0F - ((borderSize + 1) / rect.Height);
+
+                        transform.Scale(scaleX, scaleY);
+                        transform.Translate(borderSize / 1.6F, borderSize / 1.6F);
+
+                        graph.Transform = transform;
+                        graph.DrawPath(penBorder, roundPath);
+                    }
+                }
+            }
+        }
+
+        private void controlRegionAndBorder(Control control, float radius, Graphics graph, Color borderColer)
+        {
+            using(GraphicsPath roundPath = GetRoundedPath(control.ClientRectangle, radius))
+            using(Pen penBorder = new Pen(borderColer, 1))
+            {
+                graph.SmoothingMode = SmoothingMode.AntiAlias;
+                control.Region = new Region(roundPath);
+                graph.DrawPath(penBorder, roundPath);
+            }
+        }
+
+        private void DrawPath(Rectangle rect, Graphics graph, Color color)
+        {
+            using (GraphicsPath roundPath = GetRoundedPath(rect, borderRadius))
+            using (Pen penBorder = new Pen(color, 3))
+            {
+                graph.DrawPath(penBorder, roundPath);
+            }
+        }
+
+        #region " Win APIs"
+        //Drag Form
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panelTitleBar_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        #endregion
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Lost focus");
+
+            timerFormReactivate.Start();
+            
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void panelContainer_Paint(object sender, PaintEventArgs e)
+        {
+            controlRegionAndBorder(panelContainer, borderRadius - (borderSize / 2.0F), e.Graphics, borderColor);
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+    }
+}
